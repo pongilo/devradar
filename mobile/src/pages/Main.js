eb7cfa5fda6180ef0d19119745e30f6525f2ed54
Elton Pongilo <elton.pongilo@gmail.com>
2020-01-17 22:16:15 -0300

    eslint and prettier applied to the backend, real time with socketio applied on the project

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -15,6 +15,15 @@ import {
 import { MaterialIcons } from "@expo/vector-icons";
 
 import api from "../services/api";
+import { connect, disconnect, subscribeToNewDevs } from "../services/socket";
+
+function setupWebsocket() {
+  disconnect();
+
+  const { latitude, longitude } = currentRegion;
+
+  connect(latitude, longitude, techs);
+}
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -44,6 +53,10 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -56,6 +69,7 @@ function Main({ navigation }) {
     });
 
     setDevs(response.data.devs);
+    setupWebsocket();
   }
 
   function handleRegionChange(region) {
